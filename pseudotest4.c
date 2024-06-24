37d3a1b0da379433b2ebfc009a1d83524b43fea7
LeoMagix <Dahleo08@gmail.com>
2024-06-24 07:44:16 +0100

    add pseudotest4 file

---
+++ pseudotest4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * I don't have a program description right now
+ * *Ok it's a program that replaces every third element of an arry with
+ * 	number 10
+ */
+
+int main()
+{
+	int arrtxt[13] = {'w', 'e', ' ', 'g', 'r','e','a','t',' ','b','o','y','!'};
+
+	int i;
+
+	for (i = 0; i < 13; ++i)
+	{
+		if (i % 3 == 0 && i != 0)
+		{
+			arrtxt[i] = 10;
+			printf("%d", arrtxt[i]);
+		}
+		else
+		{
+			printf("%c", arrtxt[i]);
+		}
+	}
+	printf("\n");
+}
