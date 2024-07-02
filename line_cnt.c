3f7f9b146172547d64470e4012eeb2f0a1890ed5
LeoMagix <Dahleo08@gmail.com>
2024-07-02 09:59:28 +0100

    Make changes to line_cnt

--- line_cnt.c
+++ line_cnt.c
@@ -4,21 +4,20 @@
  * simple program to count the number of lines
  */
 
-#define EOF	'.'	//Redefined end-of-file
+//#define EOF	'.'	//Redefined end-of-file
 
 int main()
 {
 	int c;
-	int n_line = 1;
+	int n_line = 0;
 	
 	while ((c = getchar()) != EOF)
 	{
 		if (c == '\n')
 		{
-			//n_line = 1;
 			++n_line;
 		}
 	}
 	/*Won't execute due to infinite loop induced by no EOF input*/
-	printf("Line(s):%d\n", n_line);		//Found a temporary way around the problem.
+	printf("Line(s):%d\n", n_line);		//*update:ctrl-D for EOF--Found a temporary way around the problem.
 }
