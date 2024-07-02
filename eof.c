9c7d57d201fdfacb79b5b6d910325004fc301412
LeoMagix <Dahleo08@gmail.com>
2024-07-02 08:35:46 +0100

    Fix issues in eof.c

--- eof.c
+++ eof.c
@@ -4,7 +4,7 @@
  * program to print EOF value
  */
 
-#define	EOF 	'~'	//Assigned '~' as end of file
+//#define	EOF 	'~'	//Assigned '~' as end of file
 
 int main()
 {
@@ -12,6 +12,7 @@ int main()
 
 	c = getchar() != EOF;
 
-	printf("%d\n", EOF);			//Doubt that EOF == -1
-	printf("%d\n", c);	//Prints 1 for every value, issues with value of EOF
+	printf("EOF value:%d\n", EOF);	//Doubt that EOF == -1. Update*;Enter CTRL + d for end of file character
+	printf("%d\n", c);	//Prints 1 for every value, issues with value of EOF. Update:* It was because 
+				// I did not enter EOF character.
 }
