4e60b16860aa6882dfd783da6fee159095e4f924
LeoMagix <Dahleo08@gmail.com>
2024-07-02 19:25:53 +0100

    Make whitespace characters tabs, backlash, newline visible

--- whitespace.c
+++ whitespace.c
@@ -5,57 +5,44 @@
  *	backspace, newlines to the terminal
  */
 
-#define EOF	'.'	//Redefined end of file
+//#define EOF	'.'	//Redefined end of file
 
 int main()
 {
-	int c;
+	double c;
 
 	while((c = getchar()) != EOF)
 	{
 	/*
 	 * Can't find a way to assign c a '\' xter with a letter in a single quote
-	 *  Problem:Lack complete knowledge on how to manipulate the character data type in C*/ 
+	 *  Problem:Lack complete knowledge on how to manipulate the character data type in C
+	 *  *update: Found it!*/ 
 	
+		if (c == '\t')
+		{
+			c = '\0';
+			printf("\\t");
+		}
 		if (c == '\n')
 		{
-<<<<<<< HEAD
-			c = '\b';
+			c = '\0';
 			printf("\\n");
 		}
 		if (c == '\\')
 		{
-			c = '\b';
+			c = '\0';
 			printf("\\");
 		}
-		if (c == '\t')
+		if (c == '\b')
 		{
-			c = '\b';
-			printf("\\t");
+			c == '\0';
+			printf("\b");
 		}
-		if (c == ' ')
+		else
 		{
-			c = '\b';
-			printf("\\blnk");
-=======
-			c = '\\n';
+			putchar(c);
 		}
-		if (c == '\\')
-		{
-			c = '\\';
-		}
-		if (c == '\t')
-		{
-			c = '  ';
-			//printf("\\t");
-		}
-		if (c == ' ')
-		{
-			c = '';
-			//printf("b");
->>>>>>> main
-		}
-		putchar(c);
+		//c = getchar(); weired results with this include in the program
 	}
 	printf("\n");
 }
